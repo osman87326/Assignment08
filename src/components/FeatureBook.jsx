921dc8d9bdf68831a84b5d0f1e526c98a287382a
Kazi Md Osman Goni <kaziosman873@gmail.com>
2026-05-01 11:56:54 +0600

    Navbar and footer added

---
+++ src/components/FeatureBook.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FeatureBook = () => {
+  return (
+     <div className='container mx-auto'>
+          <h1 className='text-3xl font-bold mt-5'>Featured Books</h1>  
+        </div>
+  )
+}
+
+export default FeatureBook
