7c43cb35f88116889f9d7ddd54678f6242f264cc
Kazi Md Osman Goni <kaziosman873@gmail.com>
2026-05-04 23:56:37 +0600

    final

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -6,7 +6,7 @@ import { MongoClient } from "mongodb";
 
 
 const client = new MongoClient(process.env.MONGODB_URI);
-await client.connect();
+//await client.connect();
 const db = client.db("booknest");
 
 export const auth = betterAuth({
@@ -16,7 +16,7 @@ export const auth = betterAuth({
     },
   database: mongodbAdapter(db, {
     // Optional: if you don't provide a client, database transactions won't be enabled.
-    client
+    client,
   }),
   
     socialProviders: {
